6d508c5b8d6f267d9142cc0780c07efc0e1a2a23
Felix Lidö <felix.lido@gmail.com>
2023-09-22 23:14:47 +0200

    removed and fixed tests

--- src/test/java/ActionTest.java
+++ src/test/java/ActionTest.java
@@ -17,16 +17,12 @@ public class ActionTest {
 
     @Test
     public void test3() {
-        assertTrue(TestClass.test2());
+        assertFalse(TestClass.test2());
     }
 
     @Test
     public void test4() {
-        assertEquals(TestClass.test3(4),4);
+        assertEquals(TestClass.test3(4),-4);
     }
 
-    @Test
-    public void test5() {
-        assertEquals(TestClass.test3(4),3);
-    }
 }
